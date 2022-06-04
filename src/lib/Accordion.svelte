d3b1100bfb531fbe664f3c297583ea2faa925f9e
auseidon986 <auslee986@gmail.com>
2022-06-04 11:38:41 -0400

    initial build

---
+++ src/lib/Accordion.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { onMount } from 'svelte';
+	import { tick } from "svelte";
+	import Item from './AccordionItem.svelte'
+
+	const data = {
+		'foo': 'Foo Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		'bar': 'Bar Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		'baz': 'Baz Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+	}
+
+	let current = ''
+
+	onMount(() => {
+		setTimeout(() => current = 'foo', 200)
+	});
+
+</script>
+
+<div class="accordion-wrapper">
+	<div class="accordion-wrapper__status">
+		<p>Currently selected section: <b>{current ? current : 'None'}</b> </p>
+	</div>
+	
+	<div class="accordion accordion-wrapper__accordion">
+		{#each Object.entries(data) as entry, i}
+			<Item {entry} isOpen={current == entry[0]} on:opened="{() => current = entry[0]}" on:closed="{() => current = ''}" />
+		{/each}
+	</div>
+	
+</div>
+
+<style>
+  .accordion-wrapper {
+    font-size: 1.6rem;
+  }
+</style>
+
---
+++ src/routes/index.svelte
@@ -0,0 +1,31 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<script>
+  import Accordion from '$lib/Accordion.svelte'
+</script>
+
+<section>
+
+  <h1>IDFive Accordion using SvelteKit</h1>
+
+  <div class="container">
+    <Accordion></Accordion>
+  </div>
+
+</section>
+
+
+<style>
+  .container {
+    max-width: 720px;
+    margin: auto;
+  }
+
+  h1 {font-size: 2rem; padding: 2rem; text-align: center;}
+</style>
